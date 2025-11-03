59d3e3f17e96392beca6089fd99a5bce6c2f6048
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-11-02 22:18:22 -0500

    working on the basic foundation of it

--- bday-project/src/App.js
+++ bday-project/src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./Styles/App.css";
 import MainPage from "./Components/MainPage.jsx";
 
 function App() {
--- bday-project/src/Components/MainPage.jsx
+++ bday-project/src/Components/MainPage.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import "../Styles/mainpage.css";   
+import { useState } from 'react'; 
+
 
 // main function to hold the birthday stuff and username etc.
-export default function MainPage() {
+export default function MainPage() { 
+
+    // instantiation for the usestate 
+    const [origState, newState] = useState('Its Your Birthday'); 
+    
+    // instantiate Birthday person variable 
+    const BirthdayPerson = 'Aneesah'; 
+
+    // create a button function for the envelope stuff 
+    // this button function is going have the Click me ! for the birthday suprise.
+    function ClickMe() { 
+        newState(
+            `Happy Birthday ${BirthdayPerson}‼️`
+        ); 
+    } 
+
   return (
     <>
-      <div>
-        <h1>Main Page for Birthday content to go in</h1>
+      <div className = 'main-div'>
+        <h1 className = 'titleDiv'>Main Page for Birthday content to go in</h1>  
+        <h2 className = 'secondtitle'>{origState}</h2>
+        <button className = 'bdaybtn' onClick={ClickMe}>Click me!</button>
       </div>
     </>
   );
