bbbd163ad623e762bbcac85a548f1700b8c63f8a
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-11-02 22:02:28 -0500

    working on this birthday project for people

---
+++ bday-project/src/Components/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';  
+
+
+// main function to hold the birthday stuff and username etc. 
+export default function MainPage() { 
+    return( 
+        <>
+        <div> 
+            <h1>Main Page for Birthday content to go in</h1>
+        </div>
+        </>
+    ); 
+} 
+
